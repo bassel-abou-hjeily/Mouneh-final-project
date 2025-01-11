dde1686163b870bb8885eb3d1f870c984279783f
basselabouhjeily <basselabouhjeily04@gmail.com>
2025-01-11 22:31:27 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ConfigProvider } from "antd";
+import "./index.css"
+import store from "./redux/store";
+import { Provider } from "react-redux";
+import { Button, message } from 'antd';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+
+  <Provider
+    store={store}>
+    <ConfigProvider
+      theme={{
+        components: {
+          Button: {
+            colorPrimary: "#b85c38", 
+            colorPrimaryHover: "#b85c38",
+            borderRadius: "2px",
+            boxShadow: "none"
+          },
+          token: {
+            borderRadius: "2px",
+            colorPrimary: "#b85c38"
+          }
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
+  </Provider>
+
+);
